b547be3436e8b225d47b691f95453648c34dc94d
zkr024 <zkr.bm.02403@gmail.com>
2022-08-17 21:39:22 -0500

    linter message

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
-# frozen_string_literal: true
+# frozen_string_literal: false
 
 class ApplicationMailer < ActionMailer::Base
   default from: 'from@example.com'
--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,4 +1,4 @@
-# frozen_string_literal: true
+# frozen_string_literal: false
 
 class ApplicationRecord < ActiveRecord::Base
   primary_abstract_class
